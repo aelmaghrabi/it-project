1f7173cf547de6d698d7cc996242b866cbdeda70
EgyTechnology <egytech2013@hotmail.com>
2021-05-28 17:32:49 +0200

    Init the Project

---
+++ assets/scripts/canvas.js
@@ -0,0 +1,15 @@
+let f = 0;
+let dir = { kk: 4, fd: 6 }
+    // for (let f of dir) {
+    //     console.log(f)
+    // }
+for (let f in dir) {
+    console.log(f)
+}
+// for (f = 0; f < 2; f++) {
+//     console.log(dir[f])
+// }
+// let hen = [5, 5]
+// for (let f in hen) {
+//     console.log(hen[f])
+// }
